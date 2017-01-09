b2c6fcaf11bedc9baf93ea63891b8d8ca8bdbbd6
bbk-hnijja01 <Shan@MacBook-Air.local>
2017-01-09 17:46:08 +0000

    full script
    full script

---
+++ fullExperimentScript.m
@@ -0,0 +1,10 @@
+
+fullNoOfRuns = 2;
+
+[trn1Res, tst1Res] = runExperiment(trn1n, tst1n, fullNoOfRuns);
+[trn2Res, tst2Res] = runExperiment(trn2n, tst2n, fullNoOfRuns);
+[trn3Res, tst3Res] = runExperiment(trn3n, tst3n, fullNoOfRuns);
+[trn4Res, tst4Res] = runExperiment(trn4n, tst4n, fullNoOfRuns);
+[trn5Res, tst5Res] = runExperiment(trn5n, tst5n, fullNoOfRuns);
+[trn6Res, tst6Res] = runExperiment(trn6n, tst6n, fullNoOfRuns);
+[trn7Res, tst7Res] = runExperiment(trn7n, tst7n, fullNoOfRuns);
